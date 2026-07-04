08de1aa0f9c2e040d35a2d8c23940ee75f392176
Zura Gloveli <zuka.gloveli24@gmail.com>
2026-07-04 07:11:47 -0400

    category image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "www.starti.ge" }],
+    remotePatterns: [
+      { protocol: "https", hostname: "www.starti.ge" },
+      { protocol: "https", hostname: "image-tether-tool.lovable.app" },
+    ],
   },
 };
 
